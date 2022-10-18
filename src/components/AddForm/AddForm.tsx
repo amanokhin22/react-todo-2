a8b93e3641dca6e9d23615d0bcaf657492b89a95
Artiom Manokhin <amanokhin22@ukr.net>
2022-10-18 16:52:04 +0300

    New Libraries, clean index and app, create components

---
+++ src/components/AddForm/AddForm.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AddForm = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default AddForm
---
+++ src/components/Logic/Logic.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import AddForm from "../AddForm/AddForm";
+import AllTodos from "../AllTodo/AllTodo";
+
+const Logic = () => {
+    return (
+        <div>
+            <div>
+            <AddForm/>
+            </div>
+            <div>
+                <AllTodos/>
+            </div>
+
+        </div>
+    )
+}
+
+export default Logic
