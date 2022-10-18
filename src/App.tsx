fdef20972f313a7b16a995c9cbb562929c37aa24
Artiom Manokhin <amanokhin22@ukr.net>
2022-10-18 17:02:34 +0300

    Fixed bug in App and create branch develop

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
     return (
         <div>
             <Routes>
-                <Route path="*/" element={<Logic/>}/>
+                <Route path="/*" element={<Logic/>}/>
             </Routes>
         </div>
     );
